9d6e826c7d34615fb3b1bf25f4a516422bcb96b2
anupam858 <thewarlord858@gmail.com>
2022-06-01 12:35:12 +0530

    basic api calls and components

---
+++ pages/posts/[slug].js
@@ -0,0 +1,76 @@
+import Head from "next/head";
+import {GraphQLClient, gql} from 'graphql-request';
+import styles from '../../styles/Slug.module.css'
+
+const graphQLCMS = new GraphQLClient(
+    "https://api-ap-south-1.graphcms.com/v2/cl3tua5xscl2l01z666kw4njj/master"
+    );
+  
+    const QUERY =  gql`
+    query Post($slug:String!){
+        post(where: {slug:$slug}){
+            id,
+            title,
+            slug,
+            datePublished,
+            author{
+                id,
+                name,
+                avatar{
+                    url
+                }
+            }
+            content{
+                html
+            }
+            coverphoto{
+                id,
+                url
+            }
+        }
+    }
+    `;
+
+    const SLUGLIST = gql `
+    {
+        posts{
+            slug
+        }
+    }
+    `
+export async function getStaticPaths(){
+    const {posts} = await graphQLCMS.request(SLUGLIST);
+    return {
+        paths: posts.map((post) => ({ params: {slug: post.slug}})),
+        fallback: false
+    }
+}
+export async function getStaticProps({params}){
+    const slug = params.slug;
+    const data = await graphQLCMS.request(QUERY, {slug});
+    const post = data.post;
+
+    return {
+        props: {
+        post,
+        },
+        revalidate: 10,
+    };
+    }
+
+export default function BlogPost({post}){
+    console.log(post)
+    return (
+        <main className={styles.Blog}>
+            <img src={post.coverphoto? post.coverphoto.url: ''} className={styles.cover} alt="" />
+            <div className={styles.title}></div>
+            <img src={post.author.avatar? post.author.avatar.url: ''} alt="" />
+            <div className={styles.authtext}>
+                <h6>By {post.author.name}</h6>
+                <h6 className={styles.date}>{post.datePublished}</h6>
+            </div>
+            <h2>{post.title}</h2>
+            <div className={styles.content} dangerouslySetInnerHTML={{__html: post.content.html}}></div>
+        </main>
+    )
+}
